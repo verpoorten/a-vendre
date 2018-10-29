e74813279fa890e92f3d17915992b56bc7ee4cc7
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2018-10-29 20:03:00 +0100

    config ok + static moved

--- avendre/wsgi.py
+++ avendre/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for todoproject project.
+WSGI config for racetiming project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "avendre.settings")
 
 application = get_wsgi_application()
+
